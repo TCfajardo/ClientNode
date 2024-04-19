c22fe73bc9a77140dc80cb9905c67fb3e6ad71d6
Leidy-Lesmes <70681138+Leidy-Lesmes@users.noreply.github.com>
2024-04-18 19:17:34 -0500

    socket

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 # app/__init__.py
 
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
 
 from app import routes
--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 # run.py
 
 import os
+
 from app import app
 
 if __name__ == '__main__':
     node_port = int(os.getenv('NODE_PORT', 5001))
     print("Node Port:", node_port)
 
-    app.run(port=node_port)
+    app.run(host='localhost', port=node_port)

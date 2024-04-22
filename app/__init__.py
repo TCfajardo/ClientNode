671984f109d4c01866ca1ca7fabcb08d1a38e565
Leidy-Lesmes <70681138+Leidy-Lesmes@users.noreply.github.com>
2024-04-22 12:44:23 -0500

    logs y cambio de hora

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
-from flask_socketio import SocketIO
 from flask_cors import CORS
+from flask_socketio import SocketIO
+
+load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/": {"origins": ""}})
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app, cors_allowed_origins='*')  # Permite CORS para WebSocket
 

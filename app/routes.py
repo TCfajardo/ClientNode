c7076b28be69f2d04c3ad4935a314cc82239527a
Leidy-Lesmes <70681138+Leidy-Lesmes@users.noreply.github.com>
2024-04-23 16:28:23 -0500

    berkeley

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,21 @@
-# routes.py
-
 import os
 import random
 import socket
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
 
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 from app import app, socketio
-simulated_start_time = None
+
+# Variable global para almacenar la hora recibida del coordinador
+coordinator_time = None
+
+# Variable global para almacenar la hora simulada
+simulated_start_time = datetime.now().strftime('%H:%M:%S')
+
+# Convertir la hora simulada a objeto datetime.time
+simulated_start_time = datetime.strptime(simulated_start_time, '%H:%M:%S').time()
 
 @app.route('/')
 def index():
@@ -21,12 +28,15 @@ def index():
     if simulated_start_time is None:
         random_seconds = random.randint(60, 120)
         add_or_subtract = random.choice([True, False])
-        
+
         if add_or_subtract:
             simulated_start_time = system_time + timedelta(seconds=random_seconds)
         else:
             simulated_start_time = system_time - timedelta(seconds=random_seconds)
-    
+
+    # Emitir la hora simulada a través del socket para que se actualice en el frontend
+    socketio.emit('simulated_time', simulated_start_time.strftime('%H:%M:%S'))
+
     # Pasar simulated_start_time como parte del contexto
     return render_template(
         'index.html', 
@@ -35,13 +45,40 @@ def index():
         system_time=system_time.strftime('%H:%M:%S'),
         simulated_time=simulated_start_time.strftime('%H:%M:%S')
     )
-    
-# Endpoint para recibir la hora del coordinador
+
+# Método para recibir la hora del coordinador
 @socketio.on('coordinator_time')
-def handle_coordinator_time(coordinator_time):
-    print('Hora del coordinador recibida:', coordinator_time)
-    coordinator_time = datetime.strptime(coordinator_time, '%d/%m/%Y, %I:%M:%S %p')
-    simulated_time = datetime.now() - coordinator_time
-    print('Diferencia entre la hora simulada y la hora del coordinador:', simulated_time)
-    # Aquí puedes emitir la diferencia a través de WebSocket si es necesario
-    # socketio.emit('difference', simulated_time)
+def handle_coordinator_time(coordinator_received_time):
+    global coordinator_time, simulated_start_time
+    
+    # Limpiar la cadena antes de analizarla
+    cleaned_time_str = coordinator_received_time.replace('\xa0', '')
+    
+    try:
+        # Analizar la hora del coordinador con el formato correcto
+        coordinator_time = time.fromisoformat(cleaned_time_str)
+        print('Hora del coordinador recibida:', coordinator_time)
+        print('Hora simulada:', simulated_start_time)
+        calculate_time_difference()
+    except ValueError as e:
+        print('Error al analizar la hora del coordinador:', e)
+
+def calculate_time_difference():
+    global simulated_start_time, coordinator_time
+    if simulated_start_time is not None and coordinator_time is not None:
+        # Obtener la fecha actual para agregarla a las horas simuladas
+        current_date = datetime.now().date()
+
+        # Convertir las horas simuladas y de coordinador en objetos datetime completos
+        simulated_datetime = datetime.combine(current_date, simulated_start_time)
+        coordinator_datetime = datetime.combine(current_date, coordinator_time)
+
+        # Calcular la diferencia entre las horas simuladas y de coordinador
+        time_difference = coordinator_datetime- simulated_datetime
+        print('Diferencia entre la hora simulada y la hora del coordinador:', time_difference)
+
+        # Emitir la diferencia calculada como un evento de socket
+        socketio.emit('time_difference', {'difference': time_difference.total_seconds()})  # Convertir a segundos
+
+    else:
+        print('No se puede calcular la diferencia porque la hora simulada o la hora del coordinador es nula.')
